5abbe817e59e756a4a9ae6fd55e2fb2805ed2cce
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-15 22:04:57 +0200

    fixed script.js file

--- js/script.js
+++ js/script.js
@@ -1,55 +1,57 @@
-let chilometri = document.querySelector(".numeroChilometri");
 
-console.log(chilometri.value + " km")
-
-let etàPasseggero = document.querySelector(".userAge");
-
-console.log(etàPasseggero.value + " anni")
-
-let chilometriInseriti = document.querySelector("span.chilometriInseriti");
-
-chilometriInseriti.innerHTML += (chilometri.value + " km");
-
-let etàInserita = document.querySelector("span.etàInserita");
-
-etàInserita.innerHTML += (etàPasseggero.value + " anni");
-
-let result = document.querySelector("div.result");
-
-let prezzoBiglietto = parseInt(0.2333 * chilometri.value);
-
-let scontoMinorenni = ((19.4 / 100) * prezzoBiglietto);
-
-let scontoOver = ((37.7 / 100) * prezzoBiglietto);
 
 let priceButton = document.querySelector("#prezzoBottone");
 
-if (etàPasseggero.value > 65) {
-
-    prezzoBiglietto -= scontoOver;
-    result.innerHTML += prezzoBiglietto.toFixed(2) + " &euro;";
-    console.log(prezzoBiglietto);
-
-} 
-else if (etàPasseggero.value < 18) {
-
-    prezzoBiglietto -= scontoMinorenni;
-    result.innerHTML += prezzoBiglietto.toFixed(2) + " &euro;";
-    console.log(prezzoBiglietto);
-} 
-else {
-    result.innerHTML += prezzoBiglietto.toFixed(2) + " &euro;";
-}
-
-let buttonCounter = 0;
-
 priceButton.addEventListener("click", function () {
+    
+    let chilometri = document.querySelector(".numeroChilometri");
+
+    console.log(chilometri.value + " km")
+
+    let etàPasseggero = document.querySelector(".userAge");
+
+    console.log(etàPasseggero.value + " anni")
+
+    let chilometriInseriti = document.querySelector("span.chilometriInseriti");
+
+    chilometriInseriti.innerHTML += (chilometri.value + " km");
+
+    let etàInserita = document.querySelector("span.etàInserita");
+
+    etàInserita.innerHTML += (etàPasseggero.value + " anni");
+
+    let result = document.querySelector("div.result");
+
+    let prezzoBiglietto = parseInt(0.2333 * chilometri.value);
+
+    let scontoMinorenni = ((19.4 / 100) * prezzoBiglietto);
+
+    let scontoOver = ((37.7 / 100) * prezzoBiglietto);
+
+    if (etàPasseggero.value > 65) {
+
+        prezzoBiglietto -= scontoOver;
+        result.innerHTML += prezzoBiglietto.toFixed(2) + " &euro;";
+        console.log(prezzoBiglietto);
+    
+    } 
+    else if (etàPasseggero.value < 18) {
+    
+        prezzoBiglietto -= scontoMinorenni;
+        result.innerHTML += prezzoBiglietto.toFixed(2) + " &euro;";
+        console.log(prezzoBiglietto);
+    } 
+    else {
+        result.innerHTML += prezzoBiglietto.toFixed(2) + " &euro;";
+    }
+
     chilometriInseriti.classList.remove("displayNone");
     etàInserita.classList.remove("displayNone");
     result.classList.remove("displayNone");
-    buttonCounter += 1
 }
 )
+
+
 
 
 
