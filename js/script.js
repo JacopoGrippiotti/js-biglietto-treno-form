2717b3363247d6ac11a4a332f2ba4b6e7b3b89bf
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-15 14:38:25 +0200

    added chilometri and etàPasseggero variables to script.js file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let chilometri = document.getElementById("numeroChilometri");
+
+let etàPasseggero = document.getElementById("userAge");
+
+console.log(etàPasseggero.value);
+
+console.log(chilometri.value);
+
